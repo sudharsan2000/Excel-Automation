3cf94f366aa04021f6159853ecb25162fedc28e3
sudharsan2000 <ssudharsan2000@gmail.com>
2020-03-23 11:02:52 +0530

    reading done

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 import pandas as pd
 
-excel = pd.ExcelFile('CO-PO MOS U15AET502 -odd 2018-19-Micro.xlsx')
 
+try:
+    excel = pd.ExcelFile('CO-PO MOS U15AET502 -odd 2018-19-Micro.xlsx')
+    INT1 = pd.read_excel(excel,'INT1') 
+    INT2 = pd.read_excel(excel,'INT2')
+except:
+    print('Error reading file')
+    exit(1)
+
+print(INT1)
