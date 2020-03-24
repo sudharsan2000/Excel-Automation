33b66f3b8bf63c91eecede2f9dd95919ddde788b
sudharsan2000 <ssudharsan2000@gmail.com>
2020-03-24 17:41:28 +0530

    something here

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import pandas as pd
+import numpy as np
 from fileWrite import * 
 
-COS={'CO1':[],'CO2':[],'CO3':[]}
-COS_count={'CO1':0,'CO2':0,'CO3':0}
+COS={'CO1':[],'CO2':[],'CO3':[],'CO4':[],'CO5':[],'CO6':[]}
+COS_count={'CO1':0,'CO2':0,'CO3':0,'CO4':0,'CO5':0,'CO6':0}
+MARKS = []
 try:
     excel = pd.ExcelFile('CO-PO MOS U15AET502 -odd 2018-19-Micro.xlsx')
     INTS = [pd.read_excel(excel,'INT1'), pd.read_excel(excel,'INT2') ]
@@ -24,6 +26,15 @@ def readInternals():
     COS['CO3'].append(INTS[0].iloc[3:,1])
     COS['CO3'].append(INTS[0].iloc[3:,2])
 
+    COS['CO4'].append(INTS[0].iloc[3:,1])
+    COS['CO4'].append(INTS[0].iloc[3:,2])
+
+    COS['CO5'].append(INTS[0].iloc[3:,1])
+    COS['CO5'].append(INTS[0].iloc[3:,2])
+
+    COS['CO6'].append(INTS[0].iloc[3:,1])
+    COS['CO6'].append(INTS[0].iloc[3:,2])
+
     for INT in INTS:
         print('Finding COs from INT',iter)
         iter+=1
@@ -39,13 +50,30 @@ def readInternals():
     print('Number of CO1 questions found : ',COS_count['CO1'])
     print('Number of CO2 questions found : ',COS_count['CO2'])
     print('Number of CO3 questions found : ',COS_count['CO3'])
+    print('Number of CO3 questions found : ',COS_count['CO4'])
+    print('Number of CO3 questions found : ',COS_count['CO5'])
+    print('Number of CO3 questions found : ',COS_count['CO6'])
+
 
 # print(COS)
+MARKS = []
+MARKS_PER_QUESTION = []
+QUESTION_ATTEMPTED = []
 def main():
+    global MARKS
     readInternals()
     # print(COS['CO1'])
-    writeToFile(r"./OP1.xlsx",COS)
+    writeToFile(r"./Course Outcomes.xlsx",COS)
+    iter = 1
 
-    
+    for flag in COS:
+        MARKS.append((pd.DataFrame(COS[flag]).to_numpy()[2:COS_count[flag] + 3,4:]).transpose() )
+        MARKS_PER_QUESTION.append((pd.DataFrame(COS[flag]).to_numpy()[2:COS_count[flag] + 3,0]).transpose() )
+
+    print(MARKS_PER_QUESTION)
+    print(np.nansum(MARKS[0],axis=1))
+    k = np.array(MARKS[0],dtype=float)
+    print(k.dtype)
+    print(np.count_nonzero(~np.isnan(k[0])))
 if( __name__ == "__main__"):
     main()
