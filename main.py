3396f6e3892022033dfc3fd28fb41dd0c776c98b
sudharsan2000 <ssudharsan2000@gmail.com>
2020-03-25 20:19:03 +0530

    DONE!

--- main.py
+++ main.py
@@ -18,9 +18,13 @@ except:
 NAMES = INTS[0].iloc[3:,2]
 ROLL_NO = INTS[0].iloc[7:,1]
 
-def appendtoframe(obj,x):
+def appendtoframe(obj,x,series=0):
+
     write = (pd.DataFrame(x)).iloc[3:,0]
-    obj.append(write)
+    if(series == 0):
+        obj.append(write)
+    else:
+        obj.append(pd.Series(x))
 
 def readInternals():
     iter = 1
@@ -70,12 +74,19 @@ def readInternals():
 
 
 # print(COS)
+NUM_STUDENTS = 0
 INT_MARKS = []
 ENDSEM_MARKS = []
 INT_MARKS_PER_QUESTION = []
 ENDSEM_MARKS_PER_QUESTION=[]
 QUESTION_ATTEMPTED = []
 GRADES = []
+EMPTY = []
+INT_NUM_GRADES = {'1':0,'2':0,'3':0}
+ENDSEM_NUM_GRADES = {'1':0,'2':0,'3':0}
+
+INT_AVERAGE_GRADE = []
+ENDSEM_AVERAGE_GRADE = []
 
 def computeGrades(x):
     ret =  []
@@ -92,6 +103,9 @@ def main():
     global ENDSEM_MARKS
     global INT_MARKS_PER_QUESTION
     global ENDSEM_MARKS_PER_QUESTION
+    global INT_NUM_GRADES
+    global ENDSEM_NUM_GRADES
+    global NUM_STUDENTS
     readInternals()
     # print(COS['CO1'])
     # writeToFile(r"./Course Outcomes.xlsx",COS)
@@ -103,7 +117,8 @@ def main():
         ENDSEM_MARKS.append((pd.DataFrame(COS[flag]).to_numpy()[INT_COS_count[flag] + 2:INT_COS_count[flag] + ENDSEM_COS_count[flag] + 4,4:]).transpose() )
         ENDSEM_MARKS_PER_QUESTION.append((pd.DataFrame(COS[flag]).to_numpy()[INT_COS_count[flag] + 2:INT_COS_count[flag] + ENDSEM_COS_count[flag] + 4,0]).transpose() )
 
-    for l,k in zip(INT_MARKS_PER_QUESTION,INT_MARKS) :
+    for l,k,m,n in zip(INT_MARKS_PER_QUESTION,INT_MARKS,ENDSEM_MARKS_PER_QUESTION, ENDSEM_MARKS) :
+        # INT
 
         l = np.array(l,dtype=float)
         k = np.array(k,dtype=float)
@@ -114,51 +129,142 @@ def main():
         PERCENTAGE = np.divide(SUM,ATTEMPTED_SUM) * 100
         GRADES = computeGrades(PERCENTAGE)
 
+        NUM_STUDENTS = np.size(GRADES)
+
         SUM = np.append([np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan],SUM)
         ATTEMPTED_SUM = np.append([np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan],ATTEMPTED_SUM)
         PERCENTAGE = np.append([np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan],PERCENTAGE)
+        EMPTY = np.ones(PERCENTAGE.shape) * np.nan
+        
+        
+        INT_NUM_GRADES['1'] = EMPTY.copy()    
+        INT_NUM_GRADES['2'] = EMPTY.copy()
+        INT_NUM_GRADES['3'] = EMPTY.copy()
+
+        ENDSEM_NUM_GRADES['1'] = EMPTY.copy()    
+        ENDSEM_NUM_GRADES['2'] = EMPTY.copy()
+        ENDSEM_NUM_GRADES['3'] = EMPTY.copy()
+        
+        INT_AVERAGE_GRADE = EMPTY.copy()
+        ENDSEM_AVERAGE_GRADE = EMPTY.copy()
+
+
+        GRADE_LIST = list(GRADES)
+        INT_NUM_GRADES['1'][1] = GRADE_LIST.count(1)
+        INT_NUM_GRADES['1'][3] = (GRADE_LIST.count(1)/NUM_STUDENTS) * 100 
+        
+        INT_NUM_GRADES['2'][1] = GRADE_LIST.count(2)
+        INT_NUM_GRADES['2'][3] = (GRADE_LIST.count(2)/NUM_STUDENTS) * 100 
+
+        INT_NUM_GRADES['3'][1] = GRADE_LIST.count(3)
+        INT_NUM_GRADES['3'][3] = (GRADE_LIST.count(3)/NUM_STUDENTS) * 100 
+
+        INT_AVERAGE_GRADE[1] = np.mean(GRADE_LIST)
+
+        INT_NUM_GRADES['3'] = np.append([np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan],INT_NUM_GRADES['3'])
+        INT_NUM_GRADES['2'] = np.append([np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan],INT_NUM_GRADES['2'])
+        INT_NUM_GRADES['1'] = np.append([np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan],INT_NUM_GRADES['1'])
+        INT_AVERAGE_GRADE = np.append([np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan],INT_AVERAGE_GRADE)
+
         GRADES = np.append([np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan],GRADES)
+
+
         appendtoframe(COS['CO' + str(iter)], SUM)
         appendtoframe(COS['CO' + str(iter)], ATTEMPTED_SUM)
         appendtoframe(COS['CO' + str(iter)], PERCENTAGE)
         appendtoframe(COS['CO' + str(iter)], GRADES)
-        # print(COS['CO' + str(iter)])
-        iter+=1
-        # print(PERCENTAGE)
-    iter = 1
-    for l,k in zip(ENDSEM_MARKS_PER_QUESTION, ENDSEM_MARKS):
+        # END SEM
+###############################################################################################################################3
+        m = np.array(m,dtype=float)
+        n = np.array(n,dtype=float)
+        SUM = np.nansum(n,axis=1)
 
-        l = np.array(l,dtype=float)
-        k = np.array(k,dtype=float)
-        SUM = np.nansum(k,axis=1)
-
-        ATTEMPTED = ~np.isnan(k)
-        ATTEMPTED_SUM =np.sum(np.multiply(ATTEMPTED,l),axis=1)
+        ATTEMPTED = ~np.isnan(n)
+        ATTEMPTED_SUM =np.sum(np.multiply(ATTEMPTED,m),axis=1)
         PERCENTAGE = np.divide(SUM,ATTEMPTED_SUM) * 100
         GRADES = computeGrades(PERCENTAGE)
 
         SUM = np.append([np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan],SUM)
         ATTEMPTED_SUM = np.append([np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan],ATTEMPTED_SUM)
         PERCENTAGE = np.append([np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan],PERCENTAGE)
+
+
+        GRADE_LIST = list(GRADES)
+        ENDSEM_NUM_GRADES['1'][1] = GRADE_LIST.count(1)
+        ENDSEM_NUM_GRADES['1'][3] = (GRADE_LIST.count(1)/NUM_STUDENTS) * 100 
+
+        ENDSEM_NUM_GRADES['2'][1] = GRADE_LIST.count(2)
+        ENDSEM_NUM_GRADES['2'][3] = (GRADE_LIST.count(2)/NUM_STUDENTS) * 100 
+
+        ENDSEM_NUM_GRADES['3'][1] = GRADE_LIST.count(3)
+        ENDSEM_NUM_GRADES['3'][3] = (GRADE_LIST.count(3)/NUM_STUDENTS) * 100 
+
+        ENDSEM_AVERAGE_GRADE[1] = np.mean(GRADE_LIST)
+
+        ENDSEM_NUM_GRADES['3'] = np.append([np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan],ENDSEM_NUM_GRADES['3'])
+        ENDSEM_NUM_GRADES['2'] = np.append([np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan],ENDSEM_NUM_GRADES['2'])
+        ENDSEM_NUM_GRADES['1'] = np.append([np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan],ENDSEM_NUM_GRADES['1'])
+        ENDSEM_AVERAGE_GRADE = np.append([np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan],ENDSEM_AVERAGE_GRADE)
+
+
+
         GRADES = np.append([np.nan,np.nan,np.nan,np.nan,np.nan,np.nan,np.nan],GRADES)
         appendtoframe(COS['CO' + str(iter)], SUM)
         appendtoframe(COS['CO' + str(iter)], ATTEMPTED_SUM)
         appendtoframe(COS['CO' + str(iter)], PERCENTAGE)
         appendtoframe(COS['CO' + str(iter)], GRADES)
-        # print(COS['CO' + str(iter)])
+
+
+        appendtoframe(COS['CO' + str(iter)], EMPTY)
+        appendtoframe(COS['CO' + str(iter)], EMPTY)  # Spacing
+        appendtoframe(COS['CO' + str(iter)], EMPTY)
+
+        
+
+        appendtoframe(COS['CO' + str(iter)], INT_NUM_GRADES['3'])
+        appendtoframe(COS['CO' + str(iter)], INT_NUM_GRADES['2'])   
+        appendtoframe(COS['CO' + str(iter)], INT_NUM_GRADES['1'])
+        appendtoframe(COS['CO' + str(iter)], INT_AVERAGE_GRADE)
+
+        appendtoframe(COS['CO' + str(iter)], ENDSEM_NUM_GRADES['3'])
+        appendtoframe(COS['CO' + str(iter)], ENDSEM_NUM_GRADES['2'])   
+        appendtoframe(COS['CO' + str(iter)], ENDSEM_NUM_GRADES['1'])
+        appendtoframe(COS['CO' + str(iter)], ENDSEM_AVERAGE_GRADE)
+
+        
+
         iter+=1
+        # print(COS['CO' + str(iter)])
         # print(PERCENTAGE)
+    
 
     for key in COS:
-        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 2][6] = 'INTERNALS Total obtained'
-        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 3][6] = 'INTERNALS Total Attempted'
-        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 4][6] = 'INTERNALS Percentage'
-        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 5][6] = 'INTERNALS Grades on\nscale of 3'
+        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 2][6] = 'INT Total obtained'
+        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 3][6] = 'INT Total Attempted'
+        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 4][6] = 'INT Percentage'
+        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 5][6] = 'INT Grades on\nscale of 3'
 
-        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 6][6] = 'ENDSEM obtained'
-        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 7][6] = 'ENDSEM Attempted'
-        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 8][6] = 'ENDSEM Percentage'
-        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 9][6] = 'ENDSEM Grades on\nscale of 3'
+
+        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 6][6] = 'END obtained'
+        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 7][6] = 'END Attempted'
+        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 8][6] = 'END Percentage'
+        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 9][6] = 'END Grades on\nscale of 3'
+
+        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 12][8] = 'Number:'
+        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 12][10] = 'Percentage:'
+        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 15][14] = 'Number of Students:'
+        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 16][14] = NUM_STUDENTS
+
+        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 13][6] = 'Total INT\nGrade 3'
+        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 14][6] = 'Total INT\nGrade 2'
+        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 15][6] = 'Total INT\nGrade 1'
+        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 16][6] = 'INT\n Avg Grade'
+
+        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 17][6] = 'Total END\nGrade 3'
+        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 18][6] = 'Total END\nGrade 2'
+        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 19][6] = 'Total END\nGrade 1'
+        COS[key][INT_COS_count[key]+ ENDSEM_COS_count[key] + 20][6] = 'END\n Avg Grade'
+
 
     writeToFile(r"./Course Outcomes.xlsx",COS)
 
